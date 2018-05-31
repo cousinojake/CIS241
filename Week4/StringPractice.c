37d1f9a3c746ae1548e3f9dfb7cef7c371632582
cousinojake <cousinoj@mail.gvsu.edu>
2018-05-31 14:26:37 -0400

    Added code in the string practice program

--- Week4/StringPractice.c
+++ Week4/StringPractice.c
@@ -3,28 +3,32 @@
 #include <stdio.h>
 
 void PrintReverse(char *string);
+void SplitStringToLine(char *string);
 
 int main()
 {
 
   // const char *testString = "Text 51.2 is the string"; //string
-  const char *testString = "-12345678abc";
-
-  // double value;
-  long value;
-  char * remainingString;
-
-  // value = strtod(testString, &remainingString); // only gets decimal if at begining of the string.
-  value = strtol(testString, &remainingString, 0); // only gets decimal if at begining of the string.
-
-  printf("TestString: %s\n", testString);
-  printf("Value of the decimal: %ld\n", value );
-  printf("Remaining string: %s\n", remainingString);
-
-  PrintReverse("abcdefghijklmnopqrst");
-  PrintReverse("Hello World");
-  PrintReverse("Jake Cousino");
+  // const char *testString = "-12345678abc";
+  //
+  // // double value;
+  // long value;
+  // char * remainingString;
+  //
+  // // value = strtod(testString, &remainingString); // only gets decimal if at begining of the string.
+  // value = strtol(testString, &remainingString, 0); // only gets decimal if at begining of the string.
+  //
+  // printf("TestString: %s\n", testString);
+  // printf("Value of the decimal: %ld\n", value );
+  // printf("Remaining string: %s\n", remainingString);
+  //
   // PrintReverse("abcdefghijklmnopqrst");
+  // PrintReverse("Hello World");
+  // PrintReverse("Jake Cousino");
+  // PrintReverse("abcdefghijklmnopqrst");
+
+  char *string = "today is nice and sunny 80f";
+  SplitStringToLine(string);
 
   return 0;
 }
@@ -48,4 +52,32 @@ void PrintReverse(char *string)
     printf("%c", string[length - i - 1]);
   }
   printf("%s\n", revString);
+}
+
+void SplitStringToLine(char *string)
+{
+  // int length = 0;
+  // for(int i = 0; i < 100; i++)
+  // {
+  //   if(string[i] == '\0')
+  //   {
+  //     break;
+  //   }
+  //   length++;
+  // }
+  //
+  // while(string[0] != '\0')
+  // {
+  //   int index = 0;
+  //   char substr[10][1];
+  //   while(string[index] != ' ')
+  //   {
+  //     substr[index][0] = string[index];
+  //     index++;
+  //   }
+  //   for(int i = index; i < length; i++)
+  //   {
+  //     string[i] = string[]
+  //   }
+  // }
 }
